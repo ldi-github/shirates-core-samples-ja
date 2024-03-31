2cd598f01df2424bb1a299b89d9c887aa7c7c9c3
ldi_nsenba <n.senba@ldi.co.jp>
2024-04-01 03:01:33 +0900

    shirates-core 6.7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-val shiratesCoreVersion = "6.7.3"
+val shiratesCoreVersion = "6.7.4"
 val appiumClientVersion = "9.1.0"
 
 val userHome = System.getProperty("user.home")
@@ -20,8 +20,8 @@ dependencies {
     testImplementation(kotlin("test"))
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
 
     // shirates-core
     testImplementation("io.github.ldi-github:shirates-core:$shiratesCoreVersion")
@@ -30,13 +30,13 @@ dependencies {
     testImplementation("io.appium:java-client:$appiumClientVersion")
 
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core
-    testImplementation("org.apache.logging.log4j:log4j-core:2.20.0")
+    testImplementation("org.apache.logging.log4j:log4j-core:2.22.1")
 
     // https://mvnrepository.com/artifact/org.slf4j/slf4j-nop
-    testImplementation("org.slf4j:slf4j-nop:2.0.5")
+    testImplementation("org.slf4j:slf4j-nop:2.0.9")
 
     // Apache Commons IO
-    testImplementation("commons-io:commons-io:2.11.0")
+    testImplementation("commons-io:commons-io:2.15.1")
 }
 
 tasks.test {
