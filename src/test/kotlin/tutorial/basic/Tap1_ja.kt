65ddd3ca0685c6db1d91c94013c8e1ff3691dc91
ldi_nsenba <n.senba@ldi.co.jp>
2024-04-28 04:45:43 +0900

    Tutorial Update tapByCoordinates

--- src/test/kotlin/tutorial/basic/Tap1_ja.kt
+++ src/test/kotlin/tutorial/basic/Tap1_ja.kt
@@ -2,6 +2,8 @@ package tutorial.basic
 
 import org.junit.jupiter.api.Test
 import shirates.core.configuration.Testrun
+import shirates.core.driver.commandextension.screenIs
+import shirates.core.driver.commandextension.select
 import shirates.core.driver.commandextension.tap
 import shirates.core.driver.commandextension.tapWithScrollDown
 import shirates.core.testcode.UITest
@@ -27,6 +29,21 @@ class Tap1_ja : UITest() {
                         .tapWithScrollDown("ロック画面")
                     it.tap("@上へ移動")
                         .tap("@上へ移動")
+                }
+            }
+        }
+    }
+
+    @Test
+    fun tapByCoordinates() {
+
+        scenario {
+            case(1) {
+                action {
+                    val e = select("ネットワークとインターネット")
+                    it.tap(x = e.bounds.centerX, y = e.bounds.centerY)
+                }.expectation {
+                    it.screenIs("[ネットワークとインターネット画面]")
                 }
             }
         }
