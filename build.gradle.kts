d53e7cfa8eabf84a83a19c7d797b62230e3eac39
ldi_nsenba <n.senba@ldi.co.jp>
2024-05-18 02:25:27 +0900

    shirates-core 7.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
-val shiratesCoreVersion = "7.0.1"
+val shiratesCoreVersion = "7.0.2"
 val appiumClientVersion = "9.1.0"
 
 val userHome = System.getProperty("user.home")
